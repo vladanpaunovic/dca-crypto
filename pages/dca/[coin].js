6b07d699553c66637426c92543a042b276461604
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-04-26 19:53:22 +0200

    chore: set footer text

--- pages/dca/[coin].js
+++ pages/dca/[coin].js
@@ -219,7 +219,7 @@ export default function Coin(props) {
       </main>
 
       <footer className="w-100 flex border-t h-20 justify-center items-center dark:bg-gray-900 dark:text-gray-100 dark:border-gray-700">
-        DCA CRYPTO
+        DCA CC - Dollar Cost Averaging Cryptocurrency
       </footer>
     </div>
   );
