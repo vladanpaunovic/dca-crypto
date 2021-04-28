d46316a707331865754fa629e75b3e014f36bcb1
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-04-28 17:06:49 +0200

    asda

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { PHASE_PRODUCTION_BUILD } = require("next/constants");
 const generateSitemap = require("./scripts/generate-sitemap.js");
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_BUILD && isServer) {
+  if (phase === PHASE_PRODUCTION_BUILD) {
     generateSitemap();
   }
 
