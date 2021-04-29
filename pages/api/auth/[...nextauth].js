efa06d13a288d795042c52917fecddf80e84fc9b
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-04-29 19:06:29 +0200

    asdas

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,9 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import User from "../../../server/models/User";
 import bcrypt from "bcrypt";
+import connectDB from "../../../server/mongodb";
 
-export default NextAuth({
+const auth = NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Credentials({
@@ -17,21 +18,24 @@ export default NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
+        console.log(credentials);
         const user = await User.findOne({ email: credentials.email });
 
-        const isMatchingUser = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
+        if (user) {
+          const isMatchingUser = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
 
-        if (isMatchingUser) {
-          return user;
-        } else {
-          return null;
-          // You can also Reject this callback with an Error or with a URL:
-          // throw new Error('error message') // Redirect to error page
-          // throw '/path/to/redirect'        // Redirect to a URL
+          if (isMatchingUser) {
+            return user;
+          }
         }
+
+        return null;
+        // You can also Reject this callback with an Error or with a URL:
+        // throw new Error('error message') // Redirect to error page
+        // throw '/path/to/redirect'        // Redirect to a URL
       },
     }),
   ],
@@ -39,3 +43,5 @@ export default NextAuth({
   // A database is optional, but required to persist accounts in a database
   //   database: process.env.DATABASE_URL,
 });
+
+export default connectDB(auth);
--- server/mongodb.js
+++ server/mongodb.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
-    // Use current db connection
     return handler(req, res);
   }
   // Use new db connection
@@ -12,6 +11,7 @@ const connectDB = (handler) => async (req, res) => {
     useCreateIndex: true,
     useNewUrlParser: true,
   });
+
   return handler(req, res);
 };
 
