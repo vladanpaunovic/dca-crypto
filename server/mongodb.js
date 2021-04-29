3d2e281a615d3960d612c4ca62d721a77743e1df
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-04-29 18:14:06 +0200

    chore: add mongo and user model

---
+++ server/mongodb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
+  // Use new db connection
+  await mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  });
+  return handler(req, res);
+};
+
+export default connectDB;
