1083aa9c809bd50dc695b0c77c1e474b1af8b515
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-05-07 22:30:06 +0200

    asdas

--- components/Dashboard/DashboardChart/DashboardChart.js
+++ components/Dashboard/DashboardChart/DashboardChart.js
@@ -15,7 +15,59 @@ import { useDashboardContext } from "../../DashboardContext/DashboardContext";
 import Loading from "../../Loading/Loading";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import dayjs from "dayjs";
+import { formatCurrency } from "@coingecko/cryptoformat";
+
 dayjs.extend(localizedFormat);
+
+const mapFormatting = (entry, bot) => {
+  switch (entry.dataKey) {
+    case "order.totalInvestment":
+    case "totalHoldings":
+    case "order.averageCost":
+    case "price":
+      return (
+        <>
+          {entry.name}: {formatCurrency(entry.value, bot.destination_currency)}
+        </>
+      );
+    case "order.balanceInQuoteCurrency": {
+      return (
+        <>
+          {entry.name}: {formatCurrency(entry.value, bot.origin_currency)}
+        </>
+      );
+    }
+    default:
+      return (
+        <>
+          {entry.name}: {entry.value}
+        </>
+      );
+  }
+};
+
+const CustomTooltip = ({ active, payload, label }) => {
+  const { state } = useDashboardContext();
+
+  if (active && payload && payload.length && state.selectedBot) {
+    return (
+      <div className="p-4 transition-shadow border rounded shadow-sm bg-white dark:bg-gray-800 dark:border-gray-800">
+        <p className="text-sm text-gray-500 dark:text-gray-200">{label}</p>
+        {payload.map((e, index) => (
+          <p
+            key={`${e.value}-${index}`}
+            style={{ color: e.color }}
+            className="text-sm"
+          >
+            {mapFormatting(e, state.selectedBot)}
+          </p>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
 
 export const CHART_SYNCID = "dashboard-chart";
 
@@ -100,22 +152,20 @@ const DashboardChart = () => {
         : {}),
     }));
 
-  console.log(match);
-
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <ResponsiveContainer>
         <AreaChart
           data={match}
           margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-          // syncId={CHART_SYNCID}
+          syncId={CHART_SYNCID}
         >
           <defs>
             <linearGradient id="price" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
             </linearGradient>
-            <linearGradient id="orderPrice" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="colorCostAverage" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
             </linearGradient>
@@ -126,52 +176,62 @@ const DashboardChart = () => {
             dataKey="price"
             stroke="#F59E0B"
             fillOpacity={0}
+            strokeWidth={2}
             fill="url(#price)"
-            name={`Price (${state.selectedBot.destination_currency})`}
+            name={`Price`}
           />
-
+          <Area
+            connectNulls
+            type="monotone"
+            stroke="#82ca9d"
+            strokeWidth={2}
+            dataKey="order.averageCost"
+            name={`Average cost`}
+            fill="url(#colorCostAverage)"
+          />
+          <Area
+            connectNulls
+            stroke="#82ca9d"
+            type="monotone"
+            dataKey="order.totalInvestment"
+            name={`Total investment`}
+            fill="url(#orderPrice)"
+          />
+          <Area
+            connectNulls
+            stroke="#82ca9d"
+            type="monotone"
+            dataKey="totalHoldings"
+            name={`Balance ${state.selectedBot.destination_currency}`}
+            fill="url(#orderPrice)"
+          />
+          <Area
+            connectNulls
+            stroke="#82ca9d"
+            type="monotone"
+            dataKey="order.balanceInQuoteCurrency"
+            name={`Balance ${state.selectedBot.origin_currency}`}
+            fill="url(#orderPrice)"
+          />
           {match.map((m) =>
             m.order ? (
               <ReferenceDot
                 key={m.order.id}
                 x={m.date}
                 y={m.price}
-                stroke="green"
+                stroke="#82ca9d"
                 r={4}
-                fill="green"
+                fill="#82ca9d"
               />
             ) : null
           )}
-
-          <Area
-            connectNulls
-            type="monotone"
-            dataKey="order.averageCost"
-            name={`Average cost (${state.selectedBot.destination_currency})`}
-            fill="url(#orderPrice)"
-          />
-
-          <Area
-            connectNulls
-            type="monotone"
-            dataKey="order.totalInvestment"
-            name={`Total investment (${state.selectedBot.destination_currency})`}
-            fill="url(#orderPrice)"
-          />
-          <Area
-            connectNulls
-            type="monotone"
-            dataKey="totalHoldings"
-            name={`Total holdings (${state.selectedBot.destination_currency})`}
-            fill="url(#orderPrice)"
-          />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="date" />
           <YAxis
             tickFormatter={(tick) => kFormatter(tick.toFixed(2))}
             dataKey="price"
           />
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
         </AreaChart>
       </ResponsiveContainer>
     </div>
