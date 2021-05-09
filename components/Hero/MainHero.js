0dabe0570cd378e91bf080f682181b3ce4efabc9
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-05-09 17:40:40 +0200

    adsada

--- components/Hero/MainHero.js
+++ components/Hero/MainHero.js
@@ -19,83 +19,84 @@ export default function Hero() {
   const randomToken = topTokens[new Date().getDay()];
 
   return (
-    <div className="relative bg-white dark:bg-gray-900 overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white dark:bg-gray-900 lg:max-w-5xl lg:w-full lg:pb-28 sm:pb-16 md:pb-20">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white dark:text-gray-900  transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-
-          <div className="relative pt-6 pl-4">
-            <nav
-              className="relative flex items-center justify-between sm:h-10"
-              aria-label="Global"
-            >
-              <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                <div className="flex items-center  md:w-auto">
-                  <Logo />
-                </div>
-              </div>
-              <div className="flex items-center justify-end mr-4">
-                <div className="mr-2">
-                  <button
-                    className="ml-2 dark:text-white text-gray hover:text-gray-900 rounded-full p-1 mr-2 focus:outline-none"
-                    onClick={() =>
-                      setTheme(theme === "light" ? "dark" : "light")
-                    }
-                  >
-                    {theme === "light" ? (
-                      <MoonIcon className="w-5 h-5" />
-                    ) : (
-                      <SunIcon className="w-5 h-5" />
-                    )}
-                  </button>
-                </div>
-                <Link href="/auth/signin?callbackUrl=/dashboard">
-                  <a className="px-2 py-1 bg-gray-100 dark:bg-gray-800 dark:text-yellow-500 rounded font-medium">
-                    Sign in
-                  </a>
-                </Link>
-              </div>
-            </nav>
-          </div>
-
-          <main className="mt-10 mx-auto max-w-7xl sm:px-6 px-4 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">
-                  Dollar-cost averaging calculator for
-                </span>{" "}
-                <span className="block text-indigo-600 dark:text-yellow-500 xl:inline">
-                  Crypto
-                </span>
-              </h1>
-              <p className="mt-3 text-base text-gray-500 dark:text-gray-200 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Calculate, visualise and understand the potential of investing
-                in cryptocurencies. People saving $50 in Bitcoin per week, over
-                the last three years turned {formatPrice(8100)} into{" "}
-                {formatPrice(56087)}
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <Link href={`/dca/${randomToken.id}`}>
-                    <a className="w-full flex items-center justify-center px-8 py-3 border dark:bg-yellow-500 dark:text-gray-900 border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                      DCA {randomToken.name}
-                    </a>
-                  </Link>
-                </div>
-              </div>
+    <>
+      <div className="relative py-3 flex justify-center">
+        <nav
+          className="relative max-w-7xl px-4 lg:px-6 w-full flex items-center justify-between sm:h-10"
+          aria-label="Global"
+        >
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div className="flex items-center  md:w-auto">
+              <Logo />
             </div>
-          </main>
-        </div>
+          </div>
+          <div className="flex items-center justify-end mr-4">
+            <div className="mr-2">
+              <button
+                className="ml-2 dark:text-white text-gray hover:text-gray-900 rounded-full p-1 mr-2 focus:outline-none"
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+              >
+                {theme === "light" ? (
+                  <MoonIcon className="w-5 h-5" />
+                ) : (
+                  <SunIcon className="w-5 h-5" />
+                )}
+              </button>
+            </div>
+            <Link href="/auth/signin?callbackUrl=/dashboard">
+              <a className="px-2 py-1 bg-gray-100 dark:bg-gray-800 dark:text-yellow-500 rounded font-medium">
+                Sign in
+              </a>
+            </Link>
+          </div>
+        </nav>
       </div>
-      <div className="lg:absolute pattern-domino lg:inset-y-0 lg:right-0 lg:w-1/3"></div>
-    </div>
+      <div className="relative bg-white dark:bg-gray-900 overflow-hidden">
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-white dark:bg-gray-900 lg:max-w-5xl lg:w-full lg:pb-28 sm:pb-16 md:pb-20">
+            <svg
+              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white dark:text-gray-900  transform translate-x-1/2"
+              fill="currentColor"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="50,0 100,0 50,100 0,100" />
+            </svg>
+
+            <div className="relative pt-6 pl-4"></div>
+
+            <main className="mt-10 mx-auto max-w-7xl sm:px-6 px-4 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28">
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">
+                    Dollar-cost averaging calculator for
+                  </span>{" "}
+                  <span className="block text-indigo-600 dark:text-yellow-500 xl:inline">
+                    Crypto
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-500 dark:text-gray-200 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                  Calculate, visualise and understand the potential of investing
+                  in cryptocurencies. People saving $50 in Bitcoin per week,
+                  over the last three years turned {formatPrice(8100)} into{" "}
+                  {formatPrice(56087)}
+                </p>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <div className="rounded-md shadow">
+                    <Link href={`/dca/${randomToken.id}`}>
+                      <a className="w-full flex items-center justify-center px-8 py-3 border dark:bg-yellow-500 dark:text-gray-900 border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                        DCA {randomToken.name}
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+        <div className="lg:absolute pattern-domino lg:inset-y-0 lg:right-0 lg:w-1/3"></div>
+      </div>
+    </>
   );
 }
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,12 +10,12 @@ const SignIn = ({ csrfToken }) => {
         <div className="max-w-md w-full space-y-8">
           <div>
             <Logo />
-            <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+            <h2 className="mt-10 text-3xl font-extrabold text-gray-900 dark:text-gray-100">
               Sign in to your account
             </h2>
           </div>
           <form
-            className="mt-8 space-y-6"
+            className="space-y-6"
             method="post"
             action="/api/auth/callback/credentials"
           >
@@ -81,15 +81,16 @@ const SignIn = ({ csrfToken }) => {
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white dark:text-gray-900 bg-indigo-600 hover:bg-indigo-700 dark:bg-yellow-500 dark:hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 dark:text-gray-900 dark:group-hover:text-gray-800 "
-                    aria-hidden="true"
-                  />
-                </span>
                 Sign in
               </button>
             </div>
+            <p className="rounded p-2 bg-gray-100 dark:bg-gray-800 dark:text-gray-400 mt-4 flex items-center">
+              <span className="p-3 bg-indigo-500 dark:bg-yellow-500 rounded mr-2">
+                <LockClosedIcon className="w-5 h-5 text-gray-200 dark:text-gray-700" />
+              </span>
+              Registration is available only by invite from existing members of
+              the platform.
+            </p>
           </form>
         </div>
       </div>
