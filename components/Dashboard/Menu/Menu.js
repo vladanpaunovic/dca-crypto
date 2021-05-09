c24ec4a3c1dd11e156424a781ef8088360cf932d
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-05-09 18:12:15 +0200

    asdas

--- components/Dashboard/Menu/Menu.js
+++ components/Dashboard/Menu/Menu.js
@@ -69,7 +69,7 @@ const Menu = () => {
             </li>
             <li className="nav-item">
               <button
-                onClick={() => signOut()}
+                onClick={() => signOut({ callbackUrl: "/" })}
                 className="text-xs px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75 "
               >
                 <LogoutIcon className="w-6 h-6 mr-1" /> Sign out
