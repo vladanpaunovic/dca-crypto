7f8e37cac84ea01013be973fae637ee03f7e3191
Vladan Paunovic <vladan.panovic@adevinta.com>
2021-05-14 17:47:44 +0200

    chore: add styles to settings page

--- components/SettingsPage/SettingsPage.js
+++ components/SettingsPage/SettingsPage.js
@@ -10,10 +10,18 @@ import {
   ExclamationIcon,
   ShieldCheckIcon,
 } from "@heroicons/react/outline";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+dayjs.extend(duration);
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
 
 const PricingPlan = (props) => {
   const [session] = useSession();
 
+  const planDuration = dayjs.duration(props.duration, "seconds").humanize();
   return (
     <div className="mx-auto bg-white h-36 w-96 rounded border-indigo-500 dark:bg-gray-800 shadow text-center p-4 mb-10">
       <div className="flex h-full items-center justify-between">
@@ -26,13 +34,8 @@ const PricingPlan = (props) => {
           </div>
           <div>
             <p className="mb-0 text-left text-gray-500 dark:text-gray-300">
-              12 of 20 user
+              {planDuration} plan
             </p>
-            <div className="w-full h-4 bg-gray-400 rounded-full mt-3">
-              <div className="w-2/3 h-full text-center text-xs text-white bg-indigo-500 rounded-full">
-                60%
-              </div>
-            </div>
           </div>
         </div>
         <div className="flex h-full flex-col justify-between">
@@ -71,33 +74,51 @@ const CurrentSubscription = (props) => {
     switch (props.paymentStatus) {
       case "charge:created":
       case "charge:pending":
-      case "charge:delayed":
         return {
           icon: <ClockIcon className="w-20 h-20" />,
           label: "Pending payment...",
           className: "bg-white dark:bg-gray-900 dark:text-gray-900",
         };
-      case "charge:confirmed":
+      case "charge:delayed":
         return {
-          icon: <ShieldCheckIcon className="w-20 h-20 dark:text-gray-900" />,
-          label: "Active until",
-          className: "bg-green-200 dark:bg-green-600 dark:text-gray-900",
+          icon: <ClockIcon className="w-20 h-20" />,
+          label: "Payment is being delayed...",
+          className: "bg-white dark:bg-gray-900 dark:text-gray-900",
+        };
+      case "charge:confirmed":
+        const subscriptionEndDate = dayjs(props.endDate).format("LL");
+        return {
+          icon: props.isActive ? (
+            <ShieldCheckIcon className="w-20 h-20 dark:text-gray-900" />
+          ) : (
+            <ExclamationIcon className="w-20 h-20 dark:text-gray-900" />
+          ),
+          label: props.isActive
+            ? `Active until ${subscriptionEndDate}`
+            : `Expired on ${subscriptionEndDate}`,
+          className: props.isActive
+            ? "bg-green-200 dark:bg-green-600 dark:text-gray-900"
+            : "bg-red-400 dark:bg-red-500",
         };
       case "charge:failed":
         return {
           icon: <ExclamationIcon className="w-20 h-20 dark:text-gray-900" />,
           label: "Payment failed",
-          className: "bg-red-400 dark:bg-red-400",
+          className: "bg-red-400 dark:bg-red-500",
         };
       default:
         return { icon: null, label: "", className: "" };
     }
   };
 
+  const planDuration = dayjs
+    .duration(props.plan.duration, "seconds")
+    .humanize();
+
   return (
     <div>
       <h3 className="mb-6 text-lg font-medium">Current subscription</h3>
-      <div className="flex bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+      <div className="flex bg-white dark:bg-gray-900 rounded-lg shadow-lg">
         <div
           className={`flex-none w-24 md:w-48 flex rounded-l-lg items-center justify-center ${
             status().className
@@ -108,17 +129,24 @@ const CurrentSubscription = (props) => {
         <form className="flex-auto p-6">
           <div className="flex flex-wrap">
             <h1 className="flex-auto text-xl font-semibold dark:text-gray-50">
-              {props.plan.name}
+              {props.plan.name} plan
             </h1>
-            <div className="text-xl font-semibold text-gray-500 dark:text-gray-300">
-              ${props.plan.price}
+            <div className="text-2xl font-semibold text-gray-500 dark:text-gray-300">
+              ${props.plan.price}{" "}
+              <span className="text-sm">{planDuration}</span>
             </div>
-            <div className="w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2">
+            <div
+              className={`w-full flex-none text-sm font-medium mt-2 ${
+                props.isActive
+                  ? "text-gray-500 dark:text-gray-300 "
+                  : "text-red-500 dark:text-red-300 "
+              }`}
+            >
               {status().label}
             </div>
           </div>
           <div className="flex items-baseline  text-sm font-medium mt-4 text-gray-700 dark:text-gray-300">
-            {props.plan.name === "Basic" && (
+            {props.plan.name === "Basic" && props.isActive && (
               <button
                 type="button"
                 className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
