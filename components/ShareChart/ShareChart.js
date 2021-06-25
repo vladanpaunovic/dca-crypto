5b68255320b860a8e21d3e71b237bbc61300a24d
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-06-25 21:25:13 +0200

    asd

--- components/ShareChart/ShareChart.js
+++ components/ShareChart/ShareChart.js
@@ -36,7 +36,7 @@ const SharingButtons = () => {
     },
     {
       label: "Facebook",
-      href: `https://facebook.com/sharer/sharer.php?u=${locationHref}`,
+      href: `https://facebook.com/sharer/sharer.php?u=${WEBSITE_URL}/dca/bitcoin`,
       color: "#3b5998",
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
@@ -48,20 +48,10 @@ const SharingButtons = () => {
     {
       label: "Reddit",
       color: "#5f99cf",
-      href: `https://reddit.com/submit/?url=${locationHref};resubmit=true&amp;title=${subject}`,
+      href: `https://reddit.com/submit/?url=${locationHref}&resubmit=true&amp&title=${subject}`,
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
           <path d="M24 11.5c0-1.65-1.35-3-3-3-.96 0-1.86.48-2.42 1.24-1.64-1-3.75-1.64-6.07-1.72.08-1.1.4-3.05 1.52-3.7.72-.4 1.73-.24 3 .5C17.2 6.3 18.46 7.5 20 7.5c1.65 0 3-1.35 3-3s-1.35-3-3-3c-1.38 0-2.54.94-2.88 2.22-1.43-.72-2.64-.8-3.6-.25-1.64.94-1.95 3.47-2 4.55-2.33.08-4.45.7-6.1 1.72C4.86 8.98 3.96 8.5 3 8.5c-1.65 0-3 1.35-3 3 0 1.32.84 2.44 2.05 2.84-.03.22-.05.44-.05.66 0 3.86 4.5 7 10 7s10-3.14 10-7c0-.22-.02-.44-.05-.66 1.2-.4 2.05-1.54 2.05-2.84zM2.3 13.37C1.5 13.07 1 12.35 1 11.5c0-1.1.9-2 2-2 .64 0 1.22.32 1.6.82-1.1.85-1.92 1.9-2.3 3.05zm3.7.13c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9.8 4.8c-1.08.63-2.42.96-3.8.96-1.4 0-2.74-.34-3.8-.95-.24-.13-.32-.44-.2-.68.15-.24.46-.32.7-.18 1.83 1.06 4.76 1.06 6.6 0 .23-.13.53-.05.67.2.14.23.06.54-.18.67zm.2-2.8c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm5.7-2.13c-.38-1.16-1.2-2.2-2.3-3.05.38-.5.97-.82 1.6-.82 1.1 0 2 .9 2 2 0 .84-.53 1.57-1.3 1.87z" />
-        </svg>
-      ),
-    },
-    {
-      label: "Pinterest",
-      color: "#bd081c",
-      href: `https://pinterest.com/pin/create/button/?url=${locationHref};description=${priceChartMessage}`,
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
-          <path d="M12.14.5C5.86.5 2.7 5 2.7 8.75c0 2.27.86 4.3 2.7 5.05.3.12.57 0 .66-.33l.27-1.06c.1-.32.06-.44-.2-.73-.52-.62-.86-1.44-.86-2.6 0-3.33 2.5-6.32 6.5-6.32 3.55 0 5.5 2.17 5.5 5.07 0 3.8-1.7 7.02-4.2 7.02-1.37 0-2.4-1.14-2.07-2.54.4-1.68 1.16-3.48 1.16-4.7 0-1.07-.58-1.98-1.78-1.98-1.4 0-2.55 1.47-2.55 3.42 0 1.25.43 2.1.43 2.1l-1.7 7.2c-.5 2.13-.08 4.75-.04 5 .02.17.22.2.3.1.14-.18 1.82-2.26 2.4-4.33.16-.58.93-3.63.93-3.63.45.88 1.8 1.65 3.22 1.65 4.25 0 7.13-3.87 7.13-9.05C20.5 4.15 17.18.5 12.14.5z" />
         </svg>
       ),
     },
@@ -69,7 +59,7 @@ const SharingButtons = () => {
     {
       label: "Linkedin",
       color: "#0077b5",
-      href: `https://www.linkedin.com/shareArticle?mini=true&amp;url=${locationHref};title=${subject};summary=${priceChartMessage}`,
+      href: `https://www.linkedin.com/shareArticle?mini=true&amp&url=${locationHref}&title=${subject}&summary=${priceChartMessage}`,
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
           <path d="M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z" />
@@ -80,23 +70,14 @@ const SharingButtons = () => {
     {
       label: "Telegram",
       color: "#54A9EB",
-      href: `https://telegram.me/share/url?text=${priceChartMessage};url=${locationHref}`,
+      href: `https://telegram.me/share/url?text=${priceChartMessage}&url=${locationHref}`,
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
           <path d="M.707 8.475C.275 8.64 0 9.508 0 9.508s.284.867.718 1.03l5.09 1.897 1.986 6.38a1.102 1.102 0 0 0 1.75.527l2.96-2.41a.405.405 0 0 1 .494-.013l5.34 3.87a1.1 1.1 0 0 0 1.046.135 1.1 1.1 0 0 0 .682-.803l3.91-18.795A1.102 1.102 0 0 0 22.5.075L.706 8.475z" />
         </svg>
       ),
     },
-    {
-      label: "WhatsApp",
-      color: "#25D366",
-      href: `whatsapp://send?text=${priceChartMessage}. ${locationHref}`,
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
-          <path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z" />
-        </svg>
-      ),
-    },
+
     {
       label: "E-Mail",
       color: "#777",
