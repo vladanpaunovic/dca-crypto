9bf78266e7117b50712153bf9d14894f20f46c91
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-07-17 17:07:59 +0200

    chore: add hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,20 @@ class MyDocument extends Document {
               gtag('config', '${GA_TRACKING_ID}');`,
             }}
           />
+
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2509319,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
         </Head>
         <body className="bg-white dark:bg-gray-900">
           <Main />
