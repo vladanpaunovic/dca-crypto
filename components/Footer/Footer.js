4cf37eb5fa2595bfbec630e20498aaf1abf329bb
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-07-18 15:44:11 +0200

    chore: add issue templates and github link

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -126,23 +126,6 @@ const Footer = ({ availableTokens }) => {
                 </li>
               </ul>
             </div>
-            <div>
-              {/* <div className="rounded-md shadow">
-                {session ? (
-                  <Link href="/dashboard">
-                    <a className="w-full py-4 px-4 bg-indigo-500 dark:bg-yellow-500 focus:ring-offset-indigo-200 text-white dark:text-gray-900 flex justify-center transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                      Go to dashboard
-                    </a>
-                  </Link>
-                ) : (
-                  <Link href="/register">
-                    <a className="w-full py-4 px-4 bg-indigo-500 dark:bg-yellow-500 focus:ring-offset-indigo-200 text-white dark:text-gray-900 flex justify-center transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                      Get Access
-                    </a>
-                  </Link>
-                )}
-              </div> */}
-            </div>
           </div>
         </div>
 
@@ -154,10 +137,22 @@ const Footer = ({ availableTokens }) => {
             <a
               target="_blank"
               href="https://twitter.com/dca_cc"
-              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              className="text-gray-500 hover:opacity-50"
             >
               <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                 <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+              </svg>
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/vladanpaunovic/dca-crypto"
+              className="text-gray-500 hover:opacity-50"
+            >
+              <svg fill="currentColor" className="h-5" viewBox="0 0 16 16">
+                <path
+                  fill="currentColor"
+                  d="M8 .2a8 8 0 00-2.53 15.59.4.4 0 00.53-.39v-1.36C3.78 14.53 3.31 13 3.31 13a2.12 2.12 0 00-.89-1.17c-.73-.5.05-.49.05-.49a1.68 1.68 0 011.23.82 1.7 1.7 0 002.3.64 1.71 1.71 0 01.51-1.07c-1.78-.2-3.64-.89-3.64-4a3.09 3.09 0 01.82-2.15 2.88 2.88 0 01.11-2.06s.67-.22 2.2.82a7.58 7.58 0 014 0c1.53-1 2.2-.82 2.2-.82a2.87 2.87 0 01.08 2.12 3.09 3.09 0 01.82 2.15c0 3.07-1.87 3.75-3.65 3.95a1.91 1.91 0 01.55 1.47v2.19a.4.4 0 00.55.38A8 8 0 008 .2z"
+                ></path>
               </svg>
             </a>
           </div>
