f951bb9ed8cbb49bfb1c9a6b59aec7092aa9f803
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-07-20 13:42:18 +0200

    chore: add url

--- server/apiClient.js
+++ server/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { WEBSITE_URL } from "../config";
 
-const apiClient = axios.create({ baseURL: `/api` });
+const apiClient = axios.create({ baseURL: `${WEBSITE_URL}/api` });
 
 export default apiClient;
