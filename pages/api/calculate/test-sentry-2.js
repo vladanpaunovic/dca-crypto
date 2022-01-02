3f34e35993965b5fce99e0816c4cb8d8d5139767
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-01-02 12:33:45 +0100

    chore: rebuild

--- pages/api/calculate/test-sentry-2.js
+++ pages/api/calculate/test-sentry-2.js
@@ -1,8 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 function handler(req) {
-  console.log("test-sentry-1", req.body.payload);
-  console.log("test-sentry-2", req.headers);
   throw new Error("Sentry from the backend");
 }
 
