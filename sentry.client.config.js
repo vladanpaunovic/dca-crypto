a2cac491a573c1a806c1fd59e1421d1aef700fd0
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-05-24 16:52:31 +0200

    chore: throw me

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,11 +3,14 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { Debug } from "@sentry/integrations";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
+  debug: true,
+  integrations: [new Debug()],
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
