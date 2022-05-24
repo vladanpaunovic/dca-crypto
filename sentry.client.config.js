189993d9c15d1648200e3bc33b5406e97ca094b3
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-05-24 13:12:49 +0200

    chore: add rrweb

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,11 +3,17 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import * as SentryRRWeb from "@sentry/rrweb";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
+  integrations: [
+    new SentryRRWeb({
+      checkoutEveryNms: 60 * 1000, // 60 seconds
+    }),
+  ],
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
@@ -15,7 +21,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 0.5,
+  tracesSampleRate: 0.3,
   beforeSend(event) {
     // Check if it is an exception, and if so, show the report dialog
     if (event.exception) {
