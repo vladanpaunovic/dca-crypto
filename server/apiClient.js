a9ebfc28f06b78abdf91f1509c323492ce215d1f
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-05-24 18:12:59 +0200

    chore: put startTransaction back

--- server/apiClient.js
+++ server/apiClient.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import * as Sentry from "@sentry/nextjs";
+import { normalize } from "@sentry/utils";
 
 import { WEBSITE_URL } from "../config";
 
@@ -7,5 +9,65 @@ const prefix = process.env.IS_PROD ? "https://" : "http://";
 const apiClient = axios.create({
   baseURL: `${prefix}${WEBSITE_URL}/api`,
 });
+
+let transaction;
+let span;
+
+apiClient.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const transactionName = `${config.method.toUpperCase()} ${config.url}`;
+    transaction = Sentry.startTransaction({
+      name: transactionName,
+      op: "http.client",
+    });
+
+    Sentry.getCurrentHub().configureScope((scope) =>
+      scope.setSpan(transaction)
+    );
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  function (response) {
+    const transactionName = `${response.config.method.toUpperCase()} ${
+      response.config.url
+    }`;
+    span = transaction.startChild({
+      data: {
+        result: response,
+      },
+      op: "http.client",
+      description: `Processing ${transactionName}`,
+    });
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    span.setHttpStatus(response.status);
+    span.finish();
+    transaction.finish();
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    span = transaction.startChild({
+      data: {
+        result: normalize(error),
+      },
+      op: "http.client",
+      description: `Processing failed`,
+    });
+
+    span.setStatus("unknown_error");
+    span.finish();
+    transaction.finish();
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
