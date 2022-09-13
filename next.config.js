ca5f8aad3c521cb7d62086ddda5b8856193c0558
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-09-13 20:53:32 +0200

    chore: cleanup

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const qs = require("query-string");
 const dayjs = require("dayjs");
 const Mode = require("frontmatter-markdown-loader/mode");
 const { PHASE_PRODUCTION_BUILD } = require("next/constants");
-
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const DEFAULT_QUERYSTRING = qs.stringify(
@@ -51,4 +46,7 @@ const moduleExports = (phase) => ({
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, { silent: true });
+module.exports = withSentryConfig(moduleExports, {
+  silent: true,
+  hideSourceMaps: false,
+});
