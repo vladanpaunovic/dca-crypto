db9b86a9028c300c2e00402fc9a3c9b2e7b7cd1f
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-10-29 12:25:26 +0200

    fix: remove test https from prefix

--- server/apiClient.js
+++ server/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { WEBSITE_URL } from "../config";
 
-const prefix = process.env.IS_PROD ? "https://" : "https://";
+const prefix = process.env.IS_PROD ? "https://" : "http://";
 
 const apiClient = axios.create({
   baseURL: `${prefix}${WEBSITE_URL}/api`,
