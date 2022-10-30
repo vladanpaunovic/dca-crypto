8d48722a6a0cc4871a1d58733bd73d314e23fe71
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-10-30 08:47:49 +0100

    chore: make logo smaller on mobile

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -6,12 +6,12 @@ const Logo = () => {
   return (
     <Link href="/">
       <a className="flex font-medium items-center border dark:border-gray-800 rounded ">
-        <div className="px-2 flex">
-          <span className="text-lg text-indigo-700 transform rotate-45 bg-white font-bold dark:bg-gray-900 dark:text-yellow-500 dark:border-gray-900">
+        <div className="px-2 py-2 flex">
+          <span className="text-lg  text-indigo-700 transform rotate-45 bg-white font-bold dark:bg-gray-900 dark:text-yellow-500 dark:border-gray-900">
             <SwitchHorizontalIcon className="w-6 h-6" />
           </span>
         </div>
-        <span className="pl-2 pr-4 py-1 text-lg bg-white font-medium dark:bg-gray-900 text-gray-900 dark:text-white dark:border-gray-900">
+        <span className="hidden sm:block pl-2 pr-4 text-lg bg-white font-medium dark:bg-gray-900 text-gray-900 dark:text-white dark:border-gray-900">
           DCA Cryptocurrency
         </span>
       </a>
