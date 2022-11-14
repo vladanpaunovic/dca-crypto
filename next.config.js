5696b4393b642e4649b36f84e3706667dd9d8c94
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-11-14 11:31:05 +0100

    chore: change sentry plugin to non silent

--- next.config.js
+++ next.config.js
@@ -73,7 +73,6 @@ const moduleExports = (phase) => ({
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(moduleExports, {
-  silent: true,
   hideSourceMaps: false,
   autoInstrumentServerFunctions: true,
 });
