beb23db68e9c654c25176af0e01571f410046c54
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-12-08 18:19:13 +0100

    chore: add blog sitemaps

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -2,9 +2,53 @@ const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const prettier = require("prettier");
+const matter = require("gray-matter");
 const getDate = new Date().toISOString();
 
 const WEBSITE_DOMAIN = "https://www.dca-cc.com";
+
+const postsDirectory = path.join(process.cwd(), "content", "blog");
+
+function getSortedPostsData() {
+  // Get file names under /posts
+  const posts = fs.readdirSync(postsDirectory);
+  const fileNames = posts.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  const allPostsData = fileNames.map((fileName) => {
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, `${fileName}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    const contents = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id: fileName,
+      ...contents.data,
+    };
+  });
+
+  // Sort posts by date
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+const blogSitemap = getSortedPostsData()
+  .map((post) => {
+    return `
+      <url>
+        <loc>${`${WEBSITE_DOMAIN}/blog/${post.id}`}</loc>
+        <lastmod>${new Date(post.date).toISOString()}</lastmod>
+      </url>`;
+  })
+  .join("");
 
 const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
 
@@ -35,7 +79,7 @@ const generateSitemaps = async () => {
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
     >
-      ${dcaListSitemap}
+      ${dcaListSitemap}${blogSitemap}
     </urlset>
   `;
 
