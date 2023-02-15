f4b2f267f67f2e406fe85b7a51604785a02e3b29
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2023-02-15 21:02:18 +0100

    lk

---
+++ scripts/sync_all_coins_description.js
@@ -0,0 +1,97 @@
+const { PrismaClient } = require("@prisma/client");
+const qs = require("qs");
+
+/** @type {import('@prisma/client').PrismaClient} */
+const prismaClient = global.prisma || new PrismaClient();
+
+const cryptoCompareFetchOptions = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.CRYPTOCOMPARE_API_KEY}`,
+  },
+};
+
+const getAllAvailableTokens = async () => {
+  const getTokensPage = async (page) => {
+    const params = qs.stringify({
+      page,
+      page_size: 100,
+      sort_by: "CREATED_ON",
+      sort_direction: "DESC",
+    });
+
+    const availableCoinsResponse = await fetch(
+      `https://data-api.cryptocompare.com/asset/v1/top/list?${params}`,
+      cryptoCompareFetchOptions
+    );
+
+    const availableCoins = await availableCoinsResponse.json();
+
+    return availableCoins;
+  };
+
+  const firstPageTokens = await getTokensPage(1); // first page is 1
+
+  const totalTokens = firstPageTokens.Data.STATS.TOTAL_ASSETS;
+  const tokens = [];
+
+  tokens.push(...firstPageTokens.Data.LIST);
+
+  for (let page = 2; page <= totalTokens / 100; page++) {
+    // for (let page = 2; page <= 3; page++) {
+    const pageTokens = await getTokensPage(page);
+    tokens.push(...pageTokens.Data.LIST);
+  }
+
+  console.log(`availableTokens: ${tokens.length}`);
+  return tokens;
+};
+
+async function main() {
+  const availableCoins = await getAllAvailableTokens();
+
+  for (let index = 0; index < availableCoins.length; index++) {
+    const coin = availableCoins[index];
+
+    const {
+      ASSET_DESCRIPTION,
+      ASSET_DESCRIPTION_SUMMARY,
+      WEBSITE_URL,
+      NAME,
+      LOGO_URL,
+      SYMBOL,
+    } = coin;
+
+    const findCoin = await prismaClient.cryptocurrency.findUnique({
+      where: { symbol: SYMBOL },
+    });
+
+    if (!findCoin) {
+      console.log(
+        `#${index + 1}/${
+          availableCoins.length
+        } - Not found in the database, skipping...`
+      );
+      continue;
+    }
+
+    console.log(
+      `#${index + 1}/${
+        availableCoins.length
+      } - Updating coin ${NAME} (${SYMBOL})`
+    );
+
+    await prismaClient.cryptocurrency.update({
+      where: { symbol: SYMBOL },
+      data: {
+        description: ASSET_DESCRIPTION,
+        descriptionSummary: ASSET_DESCRIPTION_SUMMARY,
+        websiteUrl: WEBSITE_URL,
+        image: LOGO_URL,
+      },
+    });
+  }
+}
+
+main();
