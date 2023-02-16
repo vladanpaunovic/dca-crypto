576ad4232e1c89647f7d0dc1db06ed67dba2e7b7
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2023-02-16 19:55:53 +0100

    chore: update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   // darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
