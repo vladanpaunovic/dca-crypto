6e3dd791bfd34b2e251205bb3881ff5ae7efabfe
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2023-02-16 23:16:51 +0100

    feat: add pricing on the coin page

--- components/Limit/Limit.js
+++ components/Limit/Limit.js
@@ -6,71 +6,81 @@ import { ExclamationIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 import { useSession, signIn } from "next-auth/react";
 import { useAppState } from "../../src/store/store";
+import { useQuery } from "react-query";
+import Pricing from "../Pricing/Pricing";
 
 const Limit = ({ canProceed }) => {
   const dispatch = useAppState((state) => state.dispatch);
   const session = useSession();
+  const pricing = useQuery("pricing", () =>
+    fetch("/api/billing/products").then((res) => res.json())
+  );
 
   const handleOnComplete = () => {
     dispatch({ type: ACTIONS.UPDATE_CAN_PROCEED, payload: { proceed: true } });
   };
 
   return (
-    <div className="bg-yellow-200 rounded-lg">
-      <div className="py-44 md:py-6 px-3 sm:px-6 lg:px-6 ">
-        <div className="flex flex-wrap flex-col md:flex-row items-center justify-between">
-          <div className="flex md:w-0 flex-1 items-center">
-            <span className="flex rounded-lg bg-yellow-400 p-2">
-              <ExclamationIcon
-                className="h-6 w-6 text-yellow-900"
-                aria-hidden="true"
-              />
-            </span>
-            <div className="ml-3 font-medium text-gray-900">
-              <p className="font-bold">
-                <span className="md:hidden">Limit reached.</span>
-                <span className="hidden md:inline">
-                  You reached your free limit of {FREE_TIER_CALCULATION_LIMIT}{" "}
-                  tasks per hour.
-                </span>
-              </p>
-              <p className="text-gray-900">
-                Please{" "}
-                <Link href="/pricing" className="underline">
-                  upgrade
-                </Link>{" "}
-                to continue this calculation or break for{" "}
-                <Countdown
-                  date={dayjs().add(canProceed.ttl, "seconds").toDate()}
-                  daysInHours={true}
-                  className="font-bold"
-                  onComplete={handleOnComplete}
+    <div>
+      <div className="bg-yellow-200 rounded-lg">
+        <div className="py-44 md:py-6 px-3 sm:px-6 lg:px-6 ">
+          <div className="flex flex-wrap flex-col md:flex-row items-center justify-between">
+            <div className="flex md:w-0 flex-1 items-center">
+              <span className="flex rounded-lg bg-yellow-400 p-2">
+                <ExclamationIcon
+                  className="h-6 w-6 text-yellow-900"
+                  aria-hidden="true"
                 />
-                .
-              </p>
-            </div>
-          </div>
-          <div className="order-3 grid grid-cols-2 gap-4 mt-6 md:mt-0 w-full md:w-auto">
-            <div className="col-span-2 md:col-span-1">
-              <Link
-                href="/pricing"
-                className="flex items-center justify-center rounded-md border border-transparent bg-gray-900 px-4 py-2 font-medium leading-6 text-white shadow-sm hover:opacity-80"
-              >
-                Upgrade
-              </Link>
-            </div>
-            {session.status === "unauthenticated" && (
-              <div className="col-span-2 md:col-span-1">
-                <button
-                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-400 px-4 py-2 font-medium leading-6 text-yellow-900 shadow-sm hover:opacity-80"
-                  onClick={() => signIn()}
-                >
-                  Login
-                </button>
+              </span>
+              <div className="ml-3 font-medium text-gray-900">
+                <p className="font-bold">
+                  <span className="md:hidden">Limit reached.</span>
+                  <span className="hidden md:inline">
+                    You reached your free limit of {FREE_TIER_CALCULATION_LIMIT}{" "}
+                    tasks per hour.
+                  </span>
+                </p>
+                <p className="text-gray-900">
+                  Please{" "}
+                  <Link href="/pricing" className="underline">
+                    upgrade
+                  </Link>{" "}
+                  to continue this calculation or break for{" "}
+                  <Countdown
+                    date={dayjs().add(canProceed.ttl, "seconds").toDate()}
+                    daysInHours={true}
+                    className="font-bold"
+                    onComplete={handleOnComplete}
+                  />
+                  .
+                </p>
               </div>
-            )}
+            </div>
+            <div className="order-3 grid grid-cols-2 gap-4 mt-6 md:mt-0 w-full md:w-auto">
+              <div className="col-span-2 md:col-span-1">
+                <Link
+                  href="/pricing"
+                  className="flex items-center justify-center rounded-md border border-transparent bg-gray-900 px-4 py-2 font-medium leading-6 text-white shadow-sm hover:opacity-80"
+                >
+                  Upgrade
+                </Link>
+              </div>
+              {session.status === "unauthenticated" && (
+                <div className="col-span-2 md:col-span-1">
+                  <button
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-400 px-4 py-2 font-medium leading-6 text-yellow-900 shadow-sm hover:opacity-80"
+                    onClick={() => signIn()}
+                  >
+                    Login
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
+      </div>
+      <div className="bg-white mt-8 rounded-lg pb-3">
+        <Pricing pricing={pricing.data} />
       </div>
     </div>
   );
