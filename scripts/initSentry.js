93f3507690f550a62391275499b435e52a3e8a31
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2024-09-02 09:58:54 +0200

    fix sentry

--- scripts/initSentry.js
+++ scripts/initSentry.js
@@ -1,16 +1,15 @@
 const Sentry = require("@sentry/node");
-const { PrismaClient } = require("@prisma/client");
-const Tracing = require("@sentry/tracing");
-
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
-const client = new PrismaClient();
 
 const initSentry = () => {
   Sentry.init({
     dsn: SENTRY_DSN,
     tracesSampleRate: 0.5,
-    integrations: [new Tracing.Integrations.Prisma({ client })],
+    integrations: [
+      Sentry.httpIntegration,
+      Sentry.mongoIntegration,
+      Sentry.prismaIntegration,
+    ],
   });
 
   return Sentry;
