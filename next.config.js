072901003d2889bff5bbabb205c61d28465e743b
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2024-09-03 14:26:19 +0200

    dassa

--- next.config.js
+++ next.config.js
@@ -5,64 +5,65 @@ const { withSentryConfig } = require("@sentry/nextjs");
 const { withPlausibleProxy } = require("next-plausible");
 
 /** @type {import('next').NextConfig} */
-const moduleExports = (phase) => ({
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      loader: "frontmatter-markdown-loader",
-      options: {
-        mode: [Mode.BODY],
-      },
-    });
-    return cfg;
-  },
-  images: {
-    domains: [
-      "img.clankapp.com",
-      "assets.coingecko.com",
-      "assets.coincap.io",
+const moduleExports = (phase) =>
+  withPlausibleProxy()({
+    webpack: (cfg) => {
+      cfg.module.rules.push({
+        test: /\.md$/,
+        loader: "frontmatter-markdown-loader",
+        options: {
+          mode: [Mode.BODY],
+        },
+      });
+      return cfg;
+    },
+    images: {
+      domains: [
+        "img.clankapp.com",
+        "assets.coingecko.com",
+        "assets.coincap.io",
 
-      // Google storage
-      "storage.googleapis.com",
-      "lh1.googleusercontent.com",
-      "lh2.googleusercontent.com",
-      "lh3.googleusercontent.com",
-      "lh4.googleusercontent.com",
-      "lh5.googleusercontent.com",
-      "lh6.googleusercontent.com",
-    ],
-  },
-  env: {
-    IS_PROD: phase === PHASE_PRODUCTION_BUILD,
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: `/dca/bitcoin`,
-        permanent: true,
-      },
-      {
-        source: "/lump-sum/:path*",
-        destination: "/dca/:path*",
-        permanent: true,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/sentry-tunnel",
-        destination: "https://o574491.ingest.sentry.io/api/5821539/envelope/",
-      },
-    ];
-  },
-});
+        // Google storage
+        "storage.googleapis.com",
+        "lh1.googleusercontent.com",
+        "lh2.googleusercontent.com",
+        "lh3.googleusercontent.com",
+        "lh4.googleusercontent.com",
+        "lh5.googleusercontent.com",
+        "lh6.googleusercontent.com",
+      ],
+    },
+    env: {
+      IS_PROD: phase === PHASE_PRODUCTION_BUILD,
+    },
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: `/dca/bitcoin`,
+          permanent: true,
+        },
+        {
+          source: "/lump-sum/:path*",
+          destination: "/dca/:path*",
+          permanent: true,
+        },
+      ];
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/sentry-tunnel",
+          destination: "https://o574491.ingest.sentry.io/api/5821539/envelope/",
+        },
+      ];
+    },
+  });
 
-const config = withPlausibleProxy()(moduleExports);
+// const config = withPlausibleProxy()(moduleExports);
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(config, {
+module.exports = withSentryConfig(moduleExports, {
   hideSourceMaps: false,
   autoInstrumentServerFunctions: true,
 });
